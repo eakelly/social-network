4ec854afd9b75ab2e5d3d57af69cd8bd988c1063
krithikapwork <krithika.p.work@gmail.com>
2022-06-23 21:11:24 +0000

    another set of changes for binding parameters to prevent sql injection

--- application/user_info.py
+++ application/user_info.py
@@ -1,53 +1,48 @@
 FETCH_USER_INFO = """
-    SELECT * FROM users WHERE user_id = {user_id}
+    SELECT * FROM users WHERE user_id = :id
 """
 
 FETCH_USER_FRIENDS = """
     WITH userFriends AS (
-    SELECT DISTINCT U.user_id, U.first_name, U.last_name FROM Users U, Friends F WHERE (U.user_id = F.user_id OR U.user_id = F.friend_id) AND (F.user_id = {user_id} or F.friend_id = {user_id})
+    SELECT DISTINCT U.user_id, U.first_name, U.last_name FROM Users U, Friends F WHERE (U.user_id = F.user_id OR U.user_id = F.friend_id) AND (F.user_id = :id or F.friend_id = :id)
 ), friendNames AS (
-    SELECT U.first_name, U.last_name FROM userFriends U WHERE U.user_id != {user_id}
+    SELECT U.first_name, U.last_name FROM userFriends U WHERE U.user_id != :id
 ) SELECT * FROM friendNames;
 """
 
 FETCH_USER_POSTS = """
-    SELECT * FROM posts WHERE user_id = {user_id}
+    SELECT * FROM posts WHERE user_id = :id
 """
 
 
 FETCH_USER_LOCATIONS = """
-    SELECT I.zipcode, L.country, L.state_name, L.city FROM located_in I, locations L WHERE user_id = {user_id} AND I.zipcode = L.zipcode
+    SELECT I.zipcode, L.country, L.state_name, L.city FROM located_in I, locations L WHERE user_id = :id AND I.zipcode = L.zipcode
 """
 
 FETCH_USER_PROFILES = """
-    SELECT * FROM profiles WHERE user_id = {user_id}
+    SELECT * FROM profiles WHERE user_id = :id
 """
 
 def fetch_user_info(id):
     query = FETCH_USER_INFO
-    query = query.format(user_id = id)
     return query
 
 
 def fetch_user_friends(id):
     query = FETCH_USER_FRIENDS
-    query = query.format(user_id = id)
     return query
 
 
 def fetch_user_posts(id):
     query = FETCH_USER_POSTS
-    query = query.format(user_id = id)
     return query
 
 
 def fetch_user_locations(id):
     query = FETCH_USER_LOCATIONS
-    query = query.format(user_id = id)
     return query
 
 
 def fetch_user_profiles(id):
     query = FETCH_USER_PROFILES
-    query = query.format(user_id = id)
     return query
--- application/user_profile.py
+++ application/user_profile.py
@@ -1,28 +1,25 @@
 FETCH_USER_INFO = """
-    SELECT * FROM users WHERE user_id = {user_id}
+    SELECT * FROM users WHERE user_id = :userid
 """
 
 FETCH_PROFILE_INFO = """
-    SELECT * FROM profiles WHERE user_id = {user_id} AND profile_id = {profile_id}
+    SELECT * FROM profiles WHERE user_id = :userid AND profile_id = :profileid
 """
 
 FETCH_PROFILE_POSTS = """
-    SELECT * FROM posts WHERE user_id = {user_id} AND profile_id = {profile_id}
+    SELECT * FROM posts WHERE user_id = :userid AND profile_id = :profileid
 """
 
 def fetch_user_info(userid):
     query = FETCH_USER_INFO
-    query = query.format(user_id = userid)
     return query
 
 
 def fetch_profile(userid, profileid):
     query = FETCH_PROFILE_INFO
-    query = query.format(user_id = userid, profile_id = profileid)
     return query
 
 
 def fetch_profile_posts(userid, profileid):
     query = FETCH_PROFILE_POSTS
-    query = query.format(user_id = userid, profile_id = profileid)
     return query
